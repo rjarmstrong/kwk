627e607bbd38310c004ec11226cf9c2cd9c1f048
Richard Armstrong <rjarmstrong@Richards-MacBook-Pro.local>
2016-08-21 19:47:02 +1200

    Added colour

--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 "os"
 "gopkg.in/urfave/cli.v1"
 	"fmt"
-	//"io"
+	"io"
+	"github.com/fatih/color"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.CommandNotFound = func(context *cli.Context, name string) {
 
-		fmt.Println("opening url %!", name)
+		fmt.Printf("opening url %s!\n", name)
 	}
 	//app.EnableBashCompletion = true
-	//cli.HelpPrinter = func(w io.Writer, templ string, data interface{}) {
-	//	fmt.Println("Ha HA.  I pwnd the help!!1")
-	//}
+
+	c := color.New(color.FgCyan).Add(color.Underline)
+	cli.HelpPrinter = func(w io.Writer, templ string, data interface{}) {
+		c.Printf("=== Welcome to kwk ===")
+	}
 
 	app.Commands = []cli.Command{
 		{
@@ -61,16 +64,6 @@ func main() {
 						return nil
 					},
 				},
-			},
-		},
-		{
-			Name:    "*",
-			Aliases: []string{"*"},
-			Usage:   "fallthrough",
-			HideHelp: true,
-			Action:  func(c *cli.Context) error {
-				fmt.Println("fall-through: ", c.Args().First())
-				return nil
 			},
 		},
 	}
