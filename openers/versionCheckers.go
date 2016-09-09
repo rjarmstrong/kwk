4a87310cae8c76070e486943f673485f42661a4a
Richard Armstrong <rjarmstrong@Richards-MacBook-Pro.local>
2016-09-09 19:17:32 +1200

    Added mainTemplates and versionCheckers
    Former-commit-id: a6103ab303bd31c16394a9c22b992e7dee965a6e

---
+++ openers/versionCheckers.go
@@ -0,0 +1,8 @@
+package openers
+
+const PhpVersion = `php -v`
+const RustVersion = `rustc -V`
+const PythonVersion = `python --version`
+const GoVersion = `go version`
+const NodeJsVersion = `node -v`
+const BashVersion = `/bin/bash --version`
