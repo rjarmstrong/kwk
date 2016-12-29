b1bb863526a30a06c9fd0e8f92ff28632e2b2cc6
Richard Armstrong <richard.armstrong@gimanzo.com>
2016-12-29 15:01:16 +1300

    Restored jtolds
    Former-commit-id: 8576a08cb30ecd2db5fb2760d1a2cfe512836a43
    Former-commit-id: da691382a8ed03bd6291a660bf18d63d2995d325

--- vendor/bitbucket.com/sharingmachine/kwkcli/build.sh
+++ vendor/bitbucket.com/sharingmachine/kwkcli/build.sh
@@ -2,8 +2,10 @@
 
 set -ef -o pipefail
 
+# TESTING
 go test ./app
 
+# BUILDING
 env GOOS=linux GOARCH=amd64 go build -x -o /builds/linux
 env GOOS=darwin GOARCH=amd64 go build -x -o /builds/mac
 env GOOS=windows GOARCH=amd64 go build -x -o /builds/windows
