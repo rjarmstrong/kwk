194c25d95faab85e1b5e657017ef48d69059df14
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-01-02 14:59:57 +1300

    Added remove if not exists
    Former-commit-id: 997273ea24e606dff627eb12df09747abf014636
    Former-commit-id: 97ff5090df33654b1422f985d067b740729842d7

--- ci.sh
+++ ci.sh
@@ -2,6 +2,8 @@
 
 KWK_VERSION=v1.0.9
 docker build . --rm -t kwkcli
+removeContainer
+
 docker run --name kwkcli kwkcli ls
 
 # UPLOAD
@@ -11,3 +13,12 @@ export BUILDKITE_S3_DEFAULT_REGION="us-east-1"
 export BUILDKITE_ARTIFACT_UPLOAD_DESTINATION="s3://kwk-cli/${BUILDKITE_JOB_ID}"
 
 buildkite-agent artifact upload /builds/${KWK_VERSION} s3://kwk-cli/${KWK_VERSION}
+
+removeContainer
+
+function removeContainer(){
+    lines=$(docker ps -a -q -f name=kwkcli | wc -l)
+    if [[ ${lines} > 0 ]]; then
+        docker rm -f kwkcli
+    fi
+}
