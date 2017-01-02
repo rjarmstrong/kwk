b15bbf607bcdfffb0c1eac84115b0757d7f638ed
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-01-02 13:50:37 +1300

    Updated build to create version folder directory and zipped npm folder output
    Former-commit-id: 84be2853fb7d47de37ef15cbcfe5290fd490f830
    Former-commit-id: b0aec87c0bf5c6784af963c5c1d963c940866683

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -ef -o pipefail
 
-KWK_VERSION=v1.0.8
+KWK_VERSION=v1.0.9
+ARCH=amd64
 BUILD=${BUILDKITE_BUILD_NUMBER}
 
 # TESTING
@@ -10,8 +11,10 @@ go test ./app
 
 
 # PREP OUTPUT
-output=/builds/${KWK_VERSION}
+releasePath=/builds/${KWK_VERSION}
 tmp=/builds/temp
+binPath=${releasePath}/bin
+npmPath=${releasePath}/npm
 
 if [[ -d "${tmp}" ]]; then
     rm -fr ${tmp}
@@ -19,25 +22,30 @@ else
     mkdir ${tmp}
 fi
 
-if [[ ! -d "${output}" ]]; then
-    mkdir ${output}
+if [[ ! -d "${releasePath}" ]]; then
+    mkdir ${releasePath}
 fi
 
+if [[ ! -d "${binPath}" ]]; then
+    mkdir ${binPath}
+fi
+
+if [[ ! -d "${npmPath}" ]]; then
+    mkdir ${npmPath}
+fi
 
 # COMPILING
-arch=amd64
-
 function compile(){
   os=$1
-  file="kwk-${os}-${arch}"
+  file="kwk-${os}-${ARCH}"
 
   # COMPILE
-  input=${tmp}/${file}
-  env GOOS=${os} GOARCH=${arch} go build -ldflags "-X main.version=${KWK_VERSION} -X main.build=${BUILD}" -x -o ${input}
+  binary=${tmp}/bin/${file}
+  env GOOS=${os} GOARCH=${ARCH} go build -ldflags "-X main.version=${KWK_VERSION} -X main.build=${BUILD}" -x -o ${binary}
 
   # ZIP
-  zipped=${output}/${file}.tar.gz
-  tar cvzf ${zipped} ${input}
+  zipped=${binPath}/${file}.tar.gz
+  tar cvzf ${zipped} -C ${tmp}/bin ${file}
 
   # CHECKSUM
   sha1sum ${zipped} > ${zipped}.sha1
@@ -49,19 +57,21 @@ compile darwin
 compile windows
 
 # CREATE NPM
-rm -fr /builds/npm
-mkdir /builds/npm
+npmTemp=${tmp}/npm
+rm -fr ${npmTemp}
+mkdir ${npmTemp}
 
-npm_dir=/builds/npm/${KWK_VERSION}
-mkdir ${npm_dir}
-echo $PWD
-cp -R dist/npm/. ${npm_dir}/
+cp -R dist/npm/. ${npmTemp}
+cp -R ${tmp}/bin/. ${npmTemp}/bin
+sed -i -- "s/RELEASE_VERSION/${KWK_VERSION}/" ${npmTemp}/package.json
+tree ${npmTemp}
 
-bin_dir=${npm_dir}/bin
-cp -R ${tmp}/. ${bin_dir}/
-sed -i -- "s/RELEASE_VERSION/${KWK_VERSION}/" ${npm_dir}/package.json
+# UPLOAD
+# TO COME
 
-tree ${npm_dir}
+# S3
+echo "CREATING NPM ARCHIVE"
+tar cvzf ${npmPath}/kwk-cli-npm.tar.gz -C ${npmTemp} .
 
 # CLEAN-UP
 rm -fr ${tmp}
