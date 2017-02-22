6c17d6081fec6e6c03633557457cd7f9f383de86
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-02-22 15:20:13 +1300

    Version bump
    Former-commit-id: ff252e32cdc869dcbdd14f848501fabce0d3437a [formerly 7b2455cf1d74b4701e4cc07a7343a5add25bd30f]
    Former-commit-id: c8c971d6e2c68f15cb87e6d79b8914e984750f93
    Former-commit-id: 0c3480f31c2867ad7450b6ad06198f1fa7a6926b

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.12
+KWK_VERSION=v1.2.13
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Added update notification\n"
+RELEASE_NOTES="Added typeahead to cat\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
