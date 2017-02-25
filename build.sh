fc1971772c34ccdd19186169c4fcff0e1eac1bc6
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-02-25 15:16:11 +1300

    version bump
    Former-commit-id: 1315793167f7538054a7e344c8d7e2cc0296d73e [formerly e5343cdceb4bef1fccdc9f853ae4c71200138e66]
    Former-commit-id: 3d08800764d3b6558cd5449e200562b5c41d2039
    Former-commit-id: 0eb6111d4bfda82839b88d7e8bbeb42062346452

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.15
+KWK_VERSION=v1.2.16
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="build badges\n"
+RELEASE_NOTES="ui upgrades to lists.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
