325f5755cbf3fef7d8a5b36438e1fa76ad9185b4
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-03-08 14:36:03 +1300

    vb
    Former-commit-id: 73b027c371312d88dbf5543a9ea1e2b0611e4971 [formerly c79c51b483339a87e2bd54879d712e0448ddfeae]
    Former-commit-id: 6cdcc5d42eb6a35ccf7c0b46f6bc6eb4bd977fd2
    Former-commit-id: af2045f91bbc87d5619adbda2cf5e3cccf0dec79

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.20
+KWK_VERSION=v1.2.21
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Fixed privacy display in pouches.\n"
+RELEASE_NOTES="Misc UI fixes.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
