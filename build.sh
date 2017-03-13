a49271cc1708e7673c39c0af43ab37bda0dffb3d
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-03-14 12:19:37 +1300

    Version bump
    Former-commit-id: 90ee7cfe48f7f1be45c8231abf9659354e3a119f [formerly defcd6630d10f831ef86746777be852873ce47dc]
    Former-commit-id: f92d2a714cc6a77650b2e173acd5df2543507744
    Former-commit-id: 7ee75bdb61777a82182b71febe92f10082d94645

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.25
+KWK_VERSION=v1.2.26
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Strong breadcrumbs.\n"
+RELEASE_NOTES="Linux fixes.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
