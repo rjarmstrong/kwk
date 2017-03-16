b913bc74e73b8831629a716dcd12f683b3aee216
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-03-17 11:04:12 +1300

    vb
    Former-commit-id: cdc431ba09589d90b846a35af9ac55137802c6eb [formerly bea4a6dd48857d8bcf2c36ca877415fe8737fce8]
    Former-commit-id: 06fdeeff2c78bd5e41a3936cba933fb9548f1ac9
    Former-commit-id: b614b410ba8e35fd39bc08681c0ca722c7886f86

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.28
+KWK_VERSION=v1.2.29
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Smart pouches.\n"
+RELEASE_NOTES="Sign in icon.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
