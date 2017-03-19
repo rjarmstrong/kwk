7090e0c27b739817362b36933a85b7bbeb8f9ddc
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-03-19 20:56:02 +1300

    vb
    Former-commit-id: 1175cd8a477f5e3296c6849e89e3e614102ce0cb [formerly 7e8ea57932ed4ce947700eb1bcec7bc85faf670a]
    Former-commit-id: c8603dd90a5956b9a21d2b56cf7c65b7a77d1f93
    Former-commit-id: cfc654ef36dd5e57eced5ad5b5739a05701d442d

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.32
+KWK_VERSION=v1.2.33
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Fixed preview bug.\n"
+RELEASE_NOTES="Color coding.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
