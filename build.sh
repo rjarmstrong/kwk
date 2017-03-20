bba78ae33699dc077181216c237daf745824959e
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-03-20 13:37:25 +1300

    vb
    Former-commit-id: cd020f57792f8d668050732da08cedb618bffad3 [formerly cae08fb37a3faa28a6c2cd3a0d735f462666ba75]
    Former-commit-id: 0d82b9d6afd3119da98f99536e87305b82fdcdb6
    Former-commit-id: 71144a352c43a7f03a5140f9b64bb24a2dd2e894

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -ef -o pipefail
-KWK_VERSION=v1.2.33
+KWK_VERSION=v1.2.34
 BUILD_NUMBER=$1
 RELEASE_TIME=$(date +%s)
-RELEASE_NOTES="Color coding.\n"
+RELEASE_NOTES="Recent pouches.\n"
 
 echo -e "\n\n\n**** kwk-cli build ${KWK_VERSION}+${BUILD_NUMBER} *****\n\n\n"
 
