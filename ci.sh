5a93e310d051756dc4cad90f48ec71c7ccc7a209
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-05-08 10:27:43 +1200

    Change build to target travis ci

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build . --rm -t kwkcli --build-arg BUILD_NUMBER=${BUILDKITE_BUILD_NUMBER}
+docker build . --rm -t kwkcli --build-arg BUILD_NUMBER=${TRAVIS_JOB_ID}  AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 docker rmi -f kwkcli
