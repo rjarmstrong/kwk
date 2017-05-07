7891cd726f490a7afc1ab40ae87ad41f35beceda
Richard Armstrong <richard.armstrong@gimanzo.com>
2017-05-08 10:44:38 +1200

    Fixed docker build command

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build . --rm -t kwkcli --build-arg BUILD_NUMBER=${TRAVIS_JOB_ID}  AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+docker build . --rm -t kwkcli --build-arg BUILD_NUMBER=${TRAVIS_JOB_ID} --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 docker rmi -f kwkcli
